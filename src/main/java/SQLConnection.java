f3876f13be2de6179232a47a0c44bed2d67a6dc0
AntonVasilevsky <q3antonio@gmail.com>
2023-03-19 12:43:59 +0300

    add gitignore and SQLconnection

--- src/main/java/SQLConnection.java
+++ src/main/java/SQLConnection.java
@@ -1,2 +1,19 @@
-package PACKAGE_NAME;public class SQLConnection {
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class SQLConnection {
+    public static final String URL = "jdbc:postgresql://localhost:5433/postgres";
+    public static final String USER = "postgres";
+    public static final String PASS = "qwerty11";
+
+    public static void main(String[] args) {
+        try (Connection connection = DriverManager.getConnection(URL, USER, PASS)){
+
+
+
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
 }
