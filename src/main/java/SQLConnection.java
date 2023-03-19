b9ceb8aadf5be03790a80eb0a5df344abbfed886
AntonVasilevsky <q3antonio@gmail.com>
2023-03-19 13:22:36 +0300

    insert into JDBC

--- src/main/java/SQLConnection.java
+++ src/main/java/SQLConnection.java
@@ -1,6 +1,4 @@
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
+import java.sql.*;
 
 public class SQLConnection {
     public static final String URL = "jdbc:postgresql://localhost:5433/postgres";
@@ -8,9 +6,19 @@ public class SQLConnection {
     public static final String PASS = "qwerty11";
 
     public static void main(String[] args) {
-        try (Connection connection = DriverManager.getConnection(URL, USER, PASS)){
+        try (Connection connection = DriverManager.getConnection(URL, USER, PASS);
+             Statement statement = connection.createStatement()){
 
+            statement.execute("INSERT INTO guests (name, city, age) VALUES ('Luk', 'Tatuin', '24')");
+            int a = statement.executeUpdate("update guests set age = 25 where name = 'Luk'");
+         // отправляет 1 запрос вместо 3 разных
+            statement.addBatch("INSERT INTO guests (name, city, age) VALUES ('Luk', 'Tatuin', '24')");
+            statement.addBatch("INSERT INTO guests (name, city, age) VALUES ('Tom', 'House', '4')");
+            statement.addBatch("INSERT INTO guests (name, city, age) VALUES ('Jerry', 'House', '2')");
 
+            statement.executeBatch();
+            statement.clearBatch();
+            //ResultSet resultSet = statement.executeQuery("select * from guests");
 
         } catch (SQLException e) {
             throw new RuntimeException(e);
